b19e72556726b01192ad6774cb59681dbd701fec
SSDEclipse <36139719+SSDEclipse@users.noreply.github.com>
2020-01-04 18:35:04 -0800

    Got Eccentricity calculated, some are imaginary

--- OpeningKeplerFile.py
+++ OpeningKeplerFile.py
@@ -9,12 +9,64 @@ from sklearn.cluster import KMeans
 import numpy as np
 import csv
 print('Hello there')
-filename = 'exoplanets4.csv'
-columnNames = ['loc_rowid', 'mpl_orbper', 'mpl_orbsmax', 'mpl_orbeccen', 'mpl_orbincl', 'mpl_bmassj', 'mpl_radj', 'mpl_dens', 'mst_teff', 'mst_mass', 'mst_rad', 'mpl_massj', 'mpl_masse', 'mpl_rade', 'mpl_rads', 'mpl_ratror']
+filename = 'Planets640FewColumns.csv'
+columnNames = ['pl_hostname', 'pl_letter', 'pl_name', 'pl_discmethod', 'pl_orbsmax', 'pl_orbsmaxerr1', 'pl_orbsmaxerr2', 'pl_orbsmaxlim', 'pl_orbeccen', 'pl_orbeccenerr1', 'pl_orbeccenerr2', 'pl_orbeccenlim', 'st_mass', 'st_masserr1', 'st_masserr2', 'pl_massj', 'pl_massjerr1', 'pl_massjerr2', 'pl_massjlim', 'pl_masse', 'pl_masseerr1', 'pl_masseerr2']
 data = pandas.read_csv(filename, names = columnNames)
-jupiterRadiiNan = list(data['mpl_radj'])
-rowid = list(data['loc_rowid'])
-del rowid[0:1]
-del jupiterRadiiNan[0:1]
-print(jupiterRadiiNan)
 print(data.shape)
+planetmass = list(data['pl_massjerr2'])
+starmass = list(data['pl_orbeccenerr2'])
+distance = list(data['pl_name'])
+del planetmass[0]
+del starmass[0]
+del distance[0]
+planetmassnew2 = [float (c) for c in planetmass]
+starmassnew2 = [float (c) for c in starmass]
+distancenew2 = [float (c) for c in distance]
+planetmassnew = []
+starmassnew = []
+distancenew = []
+for i in range(len(planetmassnew2)):
+    planetmassnew.append(5.989*10**24*planetmassnew2[i])
+for i in range(len(starmassnew2)):
+    starmassnew.append(1.989*10**30*starmassnew2[i])
+for i in range(len(distancenew2)):
+    distancenew.append(149597870691.*distancenew2[i])
+
+print(len(distance))
+if len(planetmassnew)==len(starmassnew)==len(distancenew):
+    print('yeet')
+StandardGravitationalParameter = []
+for i in range(len(starmassnew)):
+    StandardGravitationalParameter.append(-6.674*10**-11*starmassnew[i])
+
+def Energy (M, m, d):
+    return (6.674*10**-11)*(-1.)*M*m*(1./(2.*d))
+PlanetsOrbitalEnergy = []
+for i in range(len(starmassnew)):
+    PlanetsOrbitalEnergy.append(Energy(starmassnew[i], planetmassnew[i], distancenew[i]))
+
+def AngularMomentum (m,d,M):
+    return (m*d*(6.674*10**-11*M*(1/d))**0.5)
+PlanetsAngularMomentum = []
+for i in range(len(planetmassnew)):
+    PlanetsAngularMomentum.append(AngularMomentum(planetmassnew[i], distancenew[i], starmassnew[i]))
+print(PlanetsAngularMomentum)
+
+# print(Energy(1.989*10**30, 5.99*10**24, 1.49*10**11))
+print(len(starmassnew))
+print(len(PlanetsAngularMomentum))
+print(len(PlanetsOrbitalEnergy))
+print(len(StandardGravitationalParameter))
+PlanetsOrbitalEccentricitynoroot = []
+
+def eccentricitynoroot (e, l, m, u):
+    return (1+(2*e*(l**2))/((m**3)*(u**2)))
+for i in range(len(starmassnew)):
+    PlanetsOrbitalEccentricitynoroot.append(eccentricitynoroot(PlanetsOrbitalEnergy[i], PlanetsAngularMomentum[i], planetmassnew[i], StandardGravitationalParameter[i]))
+
+
+def eccentricity (Energy, l, m, u):
+    return (1+(2*Energy*(l**2))/((m**3)*(u**2)))**0.5
+PlanetsOrbitalEccentricity = []
+for i in range(len(starmassnew)):
+    PlanetsOrbitalEccentricity.append(eccentricity(PlanetsOrbitalEnergy[i], PlanetsAngularMomentum[i], planetmassnew[i], StandardGravitationalParameter[i]))
