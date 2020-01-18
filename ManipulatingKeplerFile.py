ed2fbed30d4b8d7893bb7184748a0f9e803983d6
SSDEclipse <36139719+SSDEclipse@users.noreply.github.com>
2020-01-18 08:47:44 -0800

    Fixed data file column names when reading in
    also got upper and lower limits for star mass, planet mass, and distance

--- ManipulatingKeplerFile.py
+++ ManipulatingKeplerFile.py
@@ -8,12 +8,12 @@ import pickle, os
 from sklearn.cluster import KMeans
 import numpy as np
 import csv
-# Loading files of 640 planets w/ mass of planet, star, and distance between them
-starmassnew = pickle.load(open(os.getcwd()+'/starmassnew', 'rb'))
-planetmassnew = pickle.load(open(os.getcwd()+'/planetmassnew', 'rb'))
-distancenew = pickle.load(open(os.getcwd()+'/distancenew', 'rb'))
-# Loading in errors for above files
+# Loading files of 640 planets w/ mass of planet, star, and distance between them - These numbers are in kilograms
+StarMass = pickle.load(open(os.getcwd()+'/starmassnew', 'rb'))
+PlanetMass = pickle.load(open(os.getcwd()+'/planetmassnew', 'rb'))
+Distance = pickle.load(open(os.getcwd()+'/distancenew', 'rb'))
 
+# Loading in errors for above files - These numbers are in Earth masses - need to convert them to kilograms to get correct error bars
 PlanetMassErrorHigh = pickle.load(open(os.getcwd()+'/PlanetMassErrorHigh', 'rb'))
 PlanetMassErrorLow = pickle.load(open(os.getcwd()+'/PlanetMassErrorLow', 'rb'))
 StarMassErrorHigh = pickle.load(open(os.getcwd()+'/StarMassErrorHigh', 'rb'))
@@ -27,23 +27,39 @@ DistanceErrorLow = pickle.load(open(os.getcwd()+'/DistanceErrorLow', 'rb'))
 # del StarMassErrorHigh[0]
 # del StarMassErrorLow[0]
 # maipulating errors to floats
-PlanetMassErrorHigh = [float (c) for c in PlanetMassErrorHigh]
-PlanetMassErrorLow = [float (c) for c in PlanetMassErrorLow]
-DistanceErrorHigh = [float (c) for c in DistanceErrorHigh]
-DistanceErrorLow = [float (c) for c in DistanceErrorLow]
-StarMassErrorHigh = [float (c) for c in StarMassErrorHigh]
-StarMassErrorLow = [float (c) for c in StarMassErrorLow]
+PlanetMassErrorHigh2 = [float (c) for c in PlanetMassErrorHigh]
+PlanetMassErrorLow2 = [float (c) for c in PlanetMassErrorLow]
+DistanceErrorHigh2 = [float (c) for c in DistanceErrorHigh]
+DistanceErrorLow2 = [float (c) for c in DistanceErrorLow]
+StarMassErrorHigh2 = [float (c) for c in StarMassErrorHigh]
+StarMassErrorLow2 = [float (c) for c in StarMassErrorLow]
+print(PlanetMassErrorHigh2)
+print(PlanetMassErrorLow2)
+PlanetMassErrorHigh =[]
+PlanetMassErrorLow =[]
+StarMassErrorHigh =[]
+StarMassErrorLow =[]
+DistanceErrorHigh =[]
+DistanceErrorLow =[]
+for i in range(len(PlanetMassErrorHigh2)):
+    PlanetMassErrorHigh.append(5.989*10**24*PlanetMassErrorHigh2[i])
+for i in range(len(PlanetMassErrorLow2)):
+    PlanetMassErrorLow.append(5.989*10**24*PlanetMassErrorLow2[i])
+for i in range(len(StarMassErrorHigh2)):
+    StarMassErrorHigh.append(1.989*10**30*StarMassErrorHigh2[i])
+for i in range(len(StarMassErrorLow2)):
+    StarMassErrorLow.append(1.989*10**30*StarMassErrorLow2[i])
+for i in range(len(DistanceErrorHigh2)):
+    DistanceErrorHigh.append(149597870691.*DistanceErrorHigh2[i])
+for i in range(len(DistanceErrorLow2)):
+    DistanceErrorLow.append(149597870691.*DistanceErrorLow2[i])
 
-PlanetMassHigh = []
-PlanetMassLow= []
-StarMassHigh= []
-StarMassLow= []
-DistanceHigh= []
-DistanceLow= []
 
-print(len(planetmassnew))
-print(len(starmassnew))
-print(len(distancenew))
+
+
+print(len(PlanetMass))
+print(len(StarMass))
+print(len(Distance))
 print(len(PlanetMassErrorHigh))
 print(len(PlanetMassErrorLow))
 print(len(StarMassErrorLow))
@@ -52,6 +68,7 @@ print(len(DistanceErrorLow))
 print(len(DistanceErrorHigh))
 print((PlanetMassErrorHigh))
 print((PlanetMassErrorLow))
+print('yeet')
 print((StarMassErrorLow))
 print((StarMassErrorHigh))
 print((DistanceErrorLow))
@@ -59,8 +76,26 @@ print((DistanceErrorHigh))
 
 
 # trying to add errors to recorded values
-for i in range(len(planetmassnew)):
-    PlanetMassHigh.append(planetmassnew[i]+PlanetMassErrorHigh[i])
+PlanetMassHigh = []
+PlanetMassLow= []
+for i in range(len(PlanetMass)):
+    PlanetMassHigh.append(PlanetMass[i]+PlanetMassErrorHigh[i])
+for i in range(len(PlanetMass)):
+    PlanetMassLow.append(PlanetMass[i]+PlanetMassErrorLow[i])
 print(PlanetMassHigh)
-print(planetmassnew)
+print(PlanetMass)
+print(PlanetMassLow)
 
+StarMassHigh= []
+StarMassLow= []
+for i in range(len(StarMass)):
+    StarMassHigh.append(StarMass[i]+StarMassErrorHigh[i])
+for i in range(len(StarMass)):
+    StarMassLow.append(StarMass[i]+StarMassErrorLow[i])
+DistanceHigh= []
+DistanceLow= []
+for i in range(len(Distance)):
+    DistanceHigh.append(Distance[i]+DistanceErrorHigh[i])
+for i in range(len(Distance)):
+    DistanceLow.append(Distance[i]+DistanceErrorLow[i])
+print ('we got em chief')
