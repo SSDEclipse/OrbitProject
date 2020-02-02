61e7bca4941b7e8643a4b794ab1139077ed4ff62
SSDEclipse <36139719+SSDEclipse@users.noreply.github.com>
2020-02-02 11:07:36 -0800

    got correct error bars to calculate eccentricity

--- EccentricityChecker.py
+++ EccentricityChecker.py
@@ -116,15 +116,20 @@ def Energy (M, m, d):
     return (6.674*10**-11)*(-1.)*M*m*(1./(2.*d))
 highenergy =[Energy(StarMassHigh[i], PlanetMassHigh[i], DistanceLow[i]) for i in range(len(PlanetMass))]
 lowenergy =[Energy(StarMassLow[i], PlanetMassLow[i], DistanceHigh[i]) for i in range(len(PlanetMass))]
+midenergy =[Energy(StarMass[i], PlanetMass[i], Distance[i]) for i in range(len(PlanetMass))]
 
 def AngularMomentum (m,d,M):
     return (m*d*(6.674*10**-11*M*(1/d))**0.5)
 highmomentum = [AngularMomentum(PlanetMassHigh[i], DistanceHigh[i], StarMassHigh[i]) for i in range(len(PlanetMass))]
 lowmomentum = [AngularMomentum(PlanetMassLow[i], DistanceLow[i], StarMassLow[i]) for i in range(len(PlanetMass))]
+midmomentum = [AngularMomentum(PlanetMass[i], Distance[i], StarMass[i]) for i in range(len(PlanetMass))]
+
 def SGP (M):
     return -6.674*10**-11*M
 StandardGPhigh = [SGP(StarMassHigh[i]) for i in range(len(StarMass))]
 StandardGPlow = [SGP(StarMassLow[i]) for i in range(len(StarMass))]
+StandardGPmid = [SGP(StarMass[i]) for i in range(len(StarMass))]
+
 PlanetMassLow1 = []
 print('yeet')
 for i in range(len(PlanetMassLow)):
@@ -149,12 +154,17 @@ def eccentricitynoroot (Energy, l, m, u):
     return (1+(2*Energy*(l**2))/((m**3)*(u**2)))
 
 higheccentricity = [eccentricitynoroot(lowenergy[i], lowmomentum[i], PlanetMassHigh[i], StandardGPhigh[i]) for i in range(len(PlanetMassHigh))]
+mideccentricity = [eccentricitynoroot(midenergy[i], midmomentum[i], PlanetMass[i], StandardGPmid[i]) for i in range(len(PlanetMassHigh))]
 loweccentricity = [eccentricitynoroot(highenergy[i], highmomentum[i], PlanetMassLow1[i], StandardGPlow[i]) for i in range(len(PlanetMassLow1))]
 higheccentricitynozeroes = []
 for i in range(len(PlanetMass)):
     if higheccentricity[i]>0:
         higheccentricitynozeroes.append((higheccentricity[i])**0.5)
 
+mideccentricitynozeroes = []
+for i in range(len(PlanetMass)):
+    if mideccentricity[i] > 0:
+            mideccentricitynozeroes.append((mideccentricity[i]) ** 0.5)
 # loweccentricitynozeroes = []
 # for i in range(len(PlanetMassLow1)):
 #     if loweccentricity[i]>0:
@@ -163,7 +173,68 @@ for i in range(len(PlanetMass)):
 print len(loweccentricity)
 print len(higheccentricitynozeroes)
 print len(higheccentricity)
-
+print (eccentricity)
+print len(mideccentricity)
+eccentricitysquared = []
+for i in range(len(eccentricity)):
+    eccentricitysquared.append(eccentricity[i]**2)
+print eccentricitysquared
+print higheccentricity
+print mideccentricity
+print eccentricityhigh
 print eccentricity
+print eccentricitylow
 # for i in range(len(PlanetMassErrorHigh)):
 #     PlanetMassErrorHigh[i]>i>PlanetMassErrorLow[i]
+print('its time for some rarted code')
+# hmm, it needs a int for the range, but i have floats
+# maybe be a big brain and mutiply all of the elements in the list by 10x so that everything becomes an int
+# find x by looking at how many siggy figgies the error bars use
+print PlanetMassErrorHigh
+
+# newlist = []
+# for i in range(len(PlanetMassErrorHigh)):
+#     newlist.append(round(PlanetMassErrorHigh[i], 3))
+# print round(PlanetMassErrorHigh[0], 0)
+# print PlanetMassErrorHigh[0]
+# x = round(1.567891234*10, 2)
+# print x
+# print newlist
+# testeccentricity = []
+# for i in range(len(lowmomentum)):
+#     for e in ((lowenergy[i]), (highenergy[i])):
+#         for l in ((lowmomentum[i]), (highmomentum[i])):
+#             for m in ((PlanetMassLow[i]), (PlanetMassHigh[i])):
+#                 for u in ((StandardGPlow[i]), (StandardGPhigh[i])):
+#                     testeccentricity.append(eccentricitynoroot(i, l, m, u))
+# for i in ((lowenergy[i]), (highenergy[i])):
+#     for l in ((lowmomentum[l]), (highmomentum[l])):
+#         for m in ((PlanetMassLow[m]), (PlanetMassHigh[m])):
+#             for u in ((StandardGPlow[u]), (StandardGPhigh[u])):
+#                 testeccentricity.append(eccentricitynoroot(i, l, m, u))
+print ((highenergy[0]-midenergy[0]))
+print ((lowenergy[0]-midenergy[0]))
+energyerrors = []
+for i in range(len(midenergy)):
+    energyerrors.append(math.fabs(highenergy[i]-midenergy[i]))
+momentumerrors = []
+for i in range(len(midmomentum)):
+    momentumerrors.append(math.fabs(highmomentum[i]-midmomentum[i]))
+StandardGPerrors = []
+for i in range(len(StandardGPmid)):
+    StandardGPerrors.append(math.fabs(StandardGPhigh[i]-StandardGPmid[i]))
+planetmasserrors = []
+for i in range(len(PlanetMass)):
+    planetmasserrors.append(math.fabs(PlanetMassHigh[i]-PlanetMass[i]))
+print energyerrors
+testeccentricity = [eccentricitynoroot(midenergy[i]+0.0005*energyerrors[i], midmomentum[i]-0.00002*momentumerrors[i], PlanetMass[i]+0.09*planetmasserrors[i], StandardGPmid[i]+0.1*StandardGPerrors[i]) for i in range(len(PlanetMassHigh))]
+print 'its testin time'
+print testeccentricity
+print eccentricitysquared
+print 'times over'
+eccentricitydifference = []
+for i in range(len(eccentricitysquared)):
+    eccentricitydifference.append(testeccentricity[i]-eccentricitysquared[i])
+print eccentricitydifference
+print 'C is an average grade'
+print ((math.fabs(sum(eccentricitydifference))/len(eccentricitydifference)))**0.5
